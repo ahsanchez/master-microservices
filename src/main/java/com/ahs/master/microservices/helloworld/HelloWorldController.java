aadc1704907be0a8cc7975a1d1b27b755121cb35
anuskinina <anuskinina@gmail.com>
2019-02-03 13:10:45 +0100

    project init

---
+++ src/main/java/com/ahs/master/microservices/helloworld/HelloWorldController.java
@@ -0,0 +1,36 @@
+package com.ahs.master.microservices.helloworld;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    
+    @Autowired
+    private MessageSource messageSource;
+    
+    @GetMapping("/hello-world")
+    public String HelloWorld(){
+        return "Hello World!";
+    }
+    
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean HelloWorldBean(){
+        return new HelloWorldBean("Hello World");
+    }
+    
+    @GetMapping("/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean HelloWorldPathVariable(
+        @PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+    
+    @GetMapping("/hello-world-internationalized")
+    public String HelloWorldInternationalized(){
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
+    }
+}
---
+++ src/main/java/com/ahs/master/microservices/user/CustomizeResponseEntityExceptionHandler.java
@@ -0,0 +1,41 @@
+package com.ahs.master.microservices.user;
+
+import java.util.Date;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+@ControllerAdvice
+@RestController
+public class CustomizeResponseEntityExceptionHandler extends ResponseEntityExceptionHandler{
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@ExceptionHandler(UserNotFoundException.class)
+	public final ResponseEntity<Object> handleAllException(UserNotFoundException ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+	}
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), "Validation failed", ex.getBindingResult().toString());
+		return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+}
